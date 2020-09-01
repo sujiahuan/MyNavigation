521397448095e6c35e067ca08dde124fe04435c4
sujiahuan <1059810269@qq.com>
2020-09-02 02:33:15 +0800

    新增了手动发功能

--- src/main/java/org/jiahuan/controller/coun/CounDataTypeController.java
+++ src/main/java/org/jiahuan/controller/coun/CounDataTypeController.java
@@ -4,8 +4,10 @@ package org.jiahuan.controller.coun;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.jiahuan.common.model.RetMsgData;
 import org.jiahuan.common.model.State;
+import org.jiahuan.common.util.DataPackageUtils;
 import org.jiahuan.common.util.VerdictUtil;
 import org.jiahuan.entity.coun.CounDataType;
+import org.jiahuan.entity.coun.CounDevice;
 import org.jiahuan.service.coun.ICounDataTypeService;
 import org.jiahuan.service.coun.ICounDeviceService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +41,53 @@ public class CounDataTypeController {
         try {
             String agreement = iCounDeviceService.getById(deviceId).getAgreement();
             iCounDataTypeService.sendRealTime(deviceId,agreement,dataType);
+            return msgData;
+        } catch (Exception e) {
+            e.printStackTrace();
+            msgData.setState(State.RET_STATE_SYSTEM_ERROR);
+            return msgData;
+        }
+    }
+
+    @GetMapping("/getRealTime")
+    public RetMsgData<String> getRealTime(@RequestParam Integer deviceId,@RequestParam Integer dataType){
+        RetMsgData<String> msgData = new RetMsgData<>();
+        try {
+            CounDevice counDevice = iCounDeviceService.getById(deviceId);
+            String dataPackage = iCounDataTypeService.getRealTimeDataPackage(counDevice, counDevice.getAgreement(), dataType, false);
+            msgData.setData(dataPackage);
+            return msgData;
+        } catch (Exception e) {
+            e.printStackTrace();
+            msgData.setState(State.RET_STATE_SYSTEM_ERROR);
+            return msgData;
+        }
+    }
+
+    @GetMapping("/computeCrc")
+    public RetMsgData<String> computeCrc(@RequestParam String msg){
+        RetMsgData<String> msgData = new RetMsgData<>();
+        try {
+            String dataPackage = DataPackageUtils.composeDataPackage(DataPackageUtils.positiveExpression(msg), false);
+            if(VerdictUtil.isNull(dataPackage)){
+                msgData.setMsg("数据格式不对");
+                return msgData;
+            }
+            msgData.setData(dataPackage);
+            return msgData;
+        } catch (Exception e) {
+            e.printStackTrace();
+            msgData.setState(State.RET_STATE_SYSTEM_ERROR);
+            return msgData;
+        }
+    }
+
+    @GetMapping("/sendMessage")
+    public RetMsgData<CounDataType> sendMessage(@RequestParam Integer deviceId,@RequestParam String msg){
+        RetMsgData<CounDataType> msgData = new RetMsgData<>();
+        try {
+            CounDevice counDevice = iCounDeviceService.getById(deviceId);
+            iCounDataTypeService.sendMessage(counDevice,msg);
             return msgData;
         } catch (Exception e) {
             e.printStackTrace();
